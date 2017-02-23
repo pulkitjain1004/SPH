b4dc7c5f9ea4e7e5e9b1f5829bb72c9943e39387
pulkit <pulkitjain1004@gmail.com>
2017-02-22 23:40:56 -0600

    Revised- use s1.lev.dist to align first 2 strings, use this profile and new string for further iteration using m.lev.dist

--- multi_pair_allign_copy.R
+++ multi_pair_allign_copy.R
@@ -64,16 +64,17 @@ while(i>1 || j>1){
       t2temp[k]=t2[j]
       k=k+1
     }
-    next
+    
   }
-  if(i==1){
+  else if(i==1){
     i=i;j=j-1
     print("left")
     t1temp[k]="_"
     t2temp[k]=t2[j]
     k=k+1
+    next
   }
-  if(j==1){
+  else{               #j==1
     i=i-1;j=j;
     print("up")
     t1temp[k]=t1[i]
@@ -156,49 +157,66 @@ m.lev.dist = function(s3, prof){
 i=m.new+1
 j=n.new+1
 k=1
-
-p1temp = matrix(0,27,max(m.new,n.new))   #R will automaticall add columns if required
+print(i)
+print(j)
+p1temp = matrix(0,27,max(m.new,n.new)+10)   #R will NOT automaticall add columns if required
 #toString(p1temp)
 rownames(p1temp) = c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_')
 
-p2temp = matrix(0,27,max(m.new,n.new))   #R will automaticall add columns if required
+p2temp = matrix(0,27,max(m.new,n.new)+10)   #R will NOT automaticall add columns if required
 #toString(p2temp)
 rownames(p2temp) = c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_')
 
 # print('Traceback direction from last character')
 
-while(i>1 || j>1){
+  while(i>1 || j>1){
   
-  if(i>1 && j>1){
-    if((D.new[i-1,j-1]<=D.new[i-1,j]) && (D.new[i-1,j-1]<=D.new[i,j-1]) ){
+    if(i>1 && j>1){
+      if((D.new[i-1,j-1]<=D.new[i-1,j]) && (D.new[i-1,j-1]<=D.new[i,j-1]) ){
     
-    #diagnol = ifelse(D[i,j]>D[i-1,j-1],0,1); 
-    print("diagonals")
-    i=i-1;j=j-1
+      #diagnol = ifelse(D[i,j]>D[i-1,j-1],0,1); 
+      
+      i=i-1;j=j-1
     
-    #p1temp[k]=t3[i]
-    pos1.temp = grep(t3.new[i],ch.list)
-    p1temp[pos1.temp,k]=p1temp[pos1.temp,k]+1
-    
-    
-    p2temp[,k]=prof[,j]
-    k=k+1
-    
-    } else if(D.new[i-1,j] < D.new[i,j-1]){
-      i=i-1;j=j;
-      print("ups")
-    
-      #t1temp[k]=t1[i]
-    
+      print("diagonals")
+      
+      #p1temp[k]=t3[i]
       pos1.temp = grep(t3.new[i],ch.list)
       p1temp[pos1.temp,k]=p1temp[pos1.temp,k]+1
     
-      p2temp[27,k]=p2temp[27,k]+1
-      k = k+1
-    } else {
+    
+      p2temp[,k]=prof[,j]
+      k=k+1
+    
+      } else if(D.new[i-1,j] < D.new[i,j-1]){
+        i=i-1;j=j;
+        print("ups")
+        
+        #t1temp[k]=t1[i]
+    
+        pos1.temp = grep(t3.new[i],ch.list)
+        p1temp[pos1.temp,k]=p1temp[pos1.temp,k]+1
+    
+        p2temp[27,k]=p2temp[27,k]+1
+        k = k+1
+      } else {
+        i=i;j=j-1
+        print("lefts")
+        
+        #t1temp[k]="_"
+        p1temp[27,k] = p1temp[27,k]+1
+    
+        #t2temp[k]=t2[j]
+        p2temp[,k]=prof[,j]
+    
+        k=k+1
+      }
+      
+    }
+    else if(i==1 & j>1){
       i=i;j=j-1
       print("lefts")
-    
+      
       #t1temp[k]="_"
       p1temp[27,k] = p1temp[27,k]+1
     
@@ -206,41 +224,31 @@ while(i>1 || j>1){
       p2temp[,k]=prof[,j]
     
       k=k+1
+      
     }
-    next
+    else{     #j==1
+      i=i-1;j=j;
+      print("ups")
+      
+      #t1temp[k]=t1[i]
+    
+      pos1.temp = grep(t3.new[i],ch.list)
+      p1temp[pos1.temp,k]=p1temp[pos1.temp,k]+1
+    
+      p2temp[27,k]=p2temp[27,k]+1
+      k = k+1
+    }
   }
-  if(i==1){
-    i=i;j=j-1
-    print("lefts")
-    
-    #t1temp[k]="_"
-    p1temp[27,k] = p1temp[27,k]+1
-    
-    #t2temp[k]=t2[j]
-    p2temp[,k]=prof[,j]
-    
-    k=k+1
-    next
-  }
-  if(j==1){
-    i=i-1;j=j;
-    print("ups")
-    
-    #t1temp[k]=t1[i]
-    
-    pos1.temp = grep(t3.new[i],ch.list)
-    p1temp[pos1.temp,k]=p1temp[pos1.temp,k]+1
-    
-    p2temp[27,k]=p2temp[27,k]+1
-    k = k+1
-  }
-}
 
   elements.new = k-1 
   prof.new = matrix(0,27,elements.new)
   rownames(prof.new) = c('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_')
 
 
+  p1temp = p1temp[,1:k-1]       #Dirty way to get rid of OUT OF BOUNDS
+  p2temp = p2temp[,1:k-1]
+  
+  
   for (i in 1:elements.new){
 
     prof.new[,i] = p1temp[,k-i] + p2temp[,k-i]
